6c3570bafe1166ff00e3351f93f194ee8146b640
lakshmanmalviya <90620208+lakshmanmalviya@users.noreply.github.com>
2021-09-17 08:17:15 +0530

    Create gram to rupee.cpp

---
+++ gram to rupee.cpp
@@ -0,0 +1,28 @@
+//  gram to rupee and rupee to gram 
+  //   convert rs to gram and gram to rs ;
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+  //  now we are going to calculate for gram to rs 
+  float productprice;
+  float gram;
+  int  yourmoney;
+  yourmoney = productprice / 1000 * gram;
+
+
+
+  cout << " Enter your productprice" << endl;
+  cin >> productprice;
+  cout << " Enter your  gram" << endl;
+  cin >> gram;
+  cout << " your money is" << yourmoney;
+
+ 
+
+  return 0;
+}
+
